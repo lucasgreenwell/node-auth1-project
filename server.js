fa246325f46577496ef508206627cec7182abe08
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-20 16:52:57 -0500

    set up session and the getusers endpoint

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors')
 const bcrypt = require('bcryptjs')
+const session = require('express-session');
 
 const server = express();
 const db = require('./data/db-config');
 
+const sessionConfig = {
+    name: 'bikini',
+    secret: 'super secret',
+    cookie: {
+        maxAge: 1000 * 3600 * 24,
+        secure: false, //needs to be true in production
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: true //gdpr compliance, illegal to set cookies automatically
+}
+
 server.use(express.json());
 server.use(cors())
+server.use(session(sessionConfig))
 
 server.post('/api/register', (req, res) => {
     if (!req.body.username || !req.body.password) {
@@ -26,7 +40,6 @@ server.post('/api/register', (req, res) => {
     }
 })
 
-//needs to be changed slightly to validate, prob the sql part mostly
 server.post('/api/login', (req, res) => {
     if (!req.body.username || !req.body.password) {
         res.status(400).json({ err: 'include all required fields' })
@@ -36,21 +49,19 @@ server.post('/api/login', (req, res) => {
                 console.log(user)
                 // res.status(200).json({mes: 'closer'})
 
-                if(user){
+                if (user) {
                     console.log(user)
-                    if (bcrypt.compareSync(req.body.password, user[0].password)){
-                        res.status(200).json({message: 'success'})
+                    if (bcrypt.compareSync(req.body.password, user[0].password)) {
+                        req.session.user =  user[0]
+                        res.status(200).json({ message: 'success' })
                     }
-                    else{
-                        res.status(403).json({message: 'invalid credentials'})
+                    else {
+                        res.status(403).json({ message: 'invalid credentials' })
                     }
                 }
-                else{
-                    res.status(403).json({message: 'invalid credentials'})
+                else {
+                    res.status(403).json({ message: 'invalid credentials' })
                 }
-
-                // if (user && bcrypt.compareSync(req.body.password, user.password)) { res.status(201).json(user) }
-                // else { res.status(401).json({ err: 'invalid credentials' }) }
             })
             .catch(err => {
                 console.log(err);
@@ -58,6 +69,26 @@ server.post('/api/login', (req, res) => {
             })
     }
 })
+
+server.get('/api/users', restricted, (req, res) => {
+    db('users').select('*')
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ err: "look, we aren't happy about this either" })
+        })
+})
+
+function restricted(req, res, next){
+    if(req.session && req.session.user){
+        next()
+    }
+    else {
+        res.status(403).json({err: "Not without a warrant you aren't"})
+    }
+} 
 
 
 
